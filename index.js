b08901082d86b7cfba8e61adf3f8689ff59d0361
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-10-28 03:30:24 +0300

    1

---
+++ index.js
@@ -0,0 +1,52 @@
+const contacts = require('./db/contacts');
+
+const { listContacts, getContactById, removeContact, addContact } = contacts; 
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case 'list':
+        const allContacts = await listContacts(); 
+        console.log(allContacts);
+        break;
+  
+      case 'get':
+         const contact = await getContactById(id);
+       console.log(contact);
+        break;
+  
+      case 'add':
+         const addedContact = await addContact(name, email, phone);
+        console.log(addedContact);
+        break;
+  
+      case 'remove':
+          const removedContact = await removeContact(id);
+        console.log(removedContact); 
+        break;
+  
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  }
+  
+  invokeAction(program.opts());
+
+
+
+
+  
